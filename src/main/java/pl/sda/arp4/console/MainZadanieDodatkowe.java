a3943503fdc667c19c4b8cf741b8280b8c8d7cb2
Oliwier <oliwiersidor@gmail.com>
2022-05-07 14:53:06 +0200

    Trzeci commit

---
+++ src/main/java/pl/sda/arp4/console/MainZadanieDodatkowe.java
@@ -0,0 +1,15 @@
+package pl.sda.arp4.console;
+
+import java.util.Scanner;
+
+public class MainZadanieDodatkowe {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("podaj początek zakresu:");
+        int liczbaA = scanner.nextInt();
+
+        System.out.println("podaj koniec zakresu");
+        int liczbaB = scanner.nextInt();
+
+    }
+}
